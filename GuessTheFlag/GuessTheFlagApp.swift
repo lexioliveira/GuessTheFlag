b0fa3ee74850faf884620a15f49e7e88cb30b980
Lexi Oliveira <lexi.m.oliveira@gmail.com>
2025-10-30 19:32:32 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Lexi Oliveira on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
